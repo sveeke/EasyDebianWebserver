82e4cfc9f297f6850c3cc470b855e7ee1a8e2acd
sveeke <mail@sebasveeke.nl>
2016-04-25 00:55:02 +0200

    Update script.sh
    Version 0.72

--- script.sh
+++ script.sh
@@ -1,30 +1,25 @@
 #!/bin/bash
+
+#############################################################################
+# Copyright 2016 Sebas Veeke
+# 
+# This file is part of SimpleDebianWebserver
 #
-# Script by Sebas Veeke
-# License: GNU General Public License version 3 (GPLv3)
+# SimpleDebianWebserver is free software: you can redistribute it and/or 
+# modify it under the terms of the GNU Affero General Public License 
+# version 3 as published by the Free Software Foundation. 
 #
+# SimpleDebianWebserver is distributed in the hope that it will be 
+# useful,but without any warranty. See the GNU Affero General Public 
+# License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License 
+# along with SimpleDebianWebserver. If not, see <http://www.gnu.org/licenses/>
+# 
 # Contact:
 # > e-mail      mail@sebasveeke.nl
 # > GitHub      sveeke
-
-
-
-### USER CONFIGURATION ###
-
-# NETWORK STUFF
-HOSTNAME=localhost                      # Hostname of the machine (for example 'webserver.sebasveeke.nl')
-FQDN="localhost"                        # Fully Qualified Domain Name (for example 'webserver.sebasveeke.nl ')
-
-# FIRST USERNAME AND PUBLIC SSH KEY
-USER1=username1
-SSH1="ssh 1234567890"
-
-# SECOND USERNAME AND PUBLIC SSH KEY
-USER2=username2
-SSH2="ssh 1234567890"
-
-# MYSQL ROOT PASSWORD
-MYSQL=password
+#############################################################################
 
 
 
@@ -33,39 +28,57 @@ MYSQL=password
 
 
 
-###############################################################################################
+
+
+#############################################################################
 
 ## Colours & markup
 
-red='\033[0;31m'        # Red
-lred='\033[1;31m'       # Lightred
-purple='\033[1;35m'     # Purple
-green='\033[0;32m'      # Green
-yellow='\033[1;33m'     # Yellow
-orange='\033[0;33m'     # Orange
-blue='\033[1;36m'       # Blue
-white='\033[1;37m'      # White
-nc='\033[0m'            # No color
-
-
-
-## Variables
-DEFAULTPASS=tijdelijk12345
+red='\033[0;31m'            # Red
+lred='\033[1;31m'           # Light red
+purple='\033[1;35m'         # Purple
+lpurple='\033[0;49;95m'     # Light purple
+lgreen='\033[0;32m'         # lgreen
+lgreen='\033[0;49;92m'      # Light lgreen
+yellow='\033[1;33m'         # Yellow
+lyellow='\033[0;49;93m'     # Light yellow
+orange='\033[0;33m'         # Orange
+blue='\033[1;36m'           # Blue
+white='\033[1;37m'          # White
+grey='\033[1;49;30m'        # Grey
+nc='\033[0m'                # No color
 
 
 
 ## Beginning script execution
 clear
 echo
+echo -e "${grey}
+#############################################################################
+# Copyright 2016 Sebas Veeke                                                #
+#                                                                           #
+# This file is part of SimpleDebianWebserver                                #
+#                                                                           #
+# SimpleDebianWebserver is free software: you can redistribute it and/or    #
+# modify it under the terms of the GNU Affero General Public License        #
+# version 3 as published by the Free Software Foundation.                   #
+#                                                                           #
+# SimpleDebianWebserver is distributed in the hope that it will be          #
+# useful,but without any warranty. See the GNU Affero General Public        #
+# License for more details.                                                 #
+#                                                                           #
+# You should have received a copy of the GNU Affero General Public License  #
+# along with SimpleDebianWebserver. If not, see http://www.gnu.org/licenses #
+#                                                                           #
+# Contact:                                                                  #
+# > e-mail      mail@sebasveeke.nl                                          #
+# > GitHub      sveeke                                                      #
+#############################################################################${nc}"
 echo
-echo -e "${yellow}This script will install and configure a webserver on your Debian 8 server."
+echo -e "${white}This script will install and configure a webserver on your Debian 8 server.${nc}"
 echo
-echo -e "***********************************************"
-echo -e "Created by Sebas Veeke"
-echo -e "Licensed under the GNU Public License version 3"
-echo -e "***********************************************"
-echo
-echo -e "Starting install script in 5 seconds. Press ctrl + c to abort."
+echo -e "${white}Starting install script in 5 seconds. Press ${lgreen}ctrl + c ${white}to abort.${nc}"
+
 sleep 5
 
 
@@ -73,7 +86,7 @@ sleep 5
 ## Requirements
 echo
 echo
-echo -e "${blue}CHECKING REQUIREMENTS"
+echo -e "${lyellow}CHECKING REQUIREMENTS"
 
 # Check if script runs as root
 echo -e -n "${white}Script is running as root..."
@@ -82,19 +95,15 @@ if [ "$EUID" -ne 0 ]; then
         echo -e "\t\t\t\t\t${white}[${red}NO${white}]${nc}"
         echo
         echo -e "${white}____________________________________________________________________________________
-
 Hi,
-
 This script should be run as root. Use sudo -s or su root and run the script again.
-
 Good luck!
-
 ____________________________________________________________________________________${nc}"
         echo
         exit
 fi
 
-echo -e "\t\t\t\t\t${white}[${green}YES${white}]${nc}"
+echo -e "\t\t\t\t\t${white}[${lgreen}YES${white}]${nc}"
 
 # Check if OS is Debian 8
 echo -e -n "${white}Checking version of Debian...${nc}"
@@ -109,40 +118,30 @@ if [[ $DEBVER != "8" ]]; then
         echo -e "\t\t\t\t\t${white}[${red}NO${white}]${nc}"
         echo
         echo -e "${white}____________________________________________________________________________________
-
 Hi,
-
 This script can only run on Debian 8, you are running $DIST.
-
 Please install Debian 8 Jessie first.
-
 Good Luck!
-
 ____________________________________________________________________________________${nc}"
         echo
         exit
 fi
 
-echo -e "\t\t\t\t\t${white}[${green}OK${white}]${nc}"
+echo -e "\t\t\t\t\t${white}[${lgreen}OK${white}]${nc}"
 
 # Checking internet connection
 echo -e -n "${white}Connected to the internet...${nc}"
 wget -q --tries=10 --timeout=20 --spider www.google.com
 if [[ $? -eq 0 ]]; then
-        echo -e "\t\t\t\t\t${white}[${green}YES${white}]${nc}"
+        echo -e "\t\t\t\t\t${white}[${lgreen}YES${white}]${nc}"
 else
         echo -e "\t\t\t\t\t${white}[${red}NO${white}]${nc}"
         echo
 echo -e "${white}____________________________________________________________________________________
-
 Hi,
-
 This script needs a functioning internet connection.
-
 Please connect to the internet first.
-
 Good Luck!
-
 ____________________________________________________________________________________${nc}"
         echo
         exit
@@ -150,212 +149,236 @@ fi
 
 sleep 1
 
+
+
+## USER CONFIGURATION ##
+echo
+echo -e "${lyellow}USER INPUT"
+echo -e "${white}The script will gather some information from you.${nc}"
+echo
+read -p "$(echo -e "${grey}Enter your username: "${lpurple})" USER
+while true
+	do
+		read -s -p "$(echo -e "${grey}Enter your password: ${nc}")" PASS
+        echo
+		read -s -p "$(echo -e "${grey}Enter your Password (again): ${nc}")" PASS2
+		[ "$PASS" = "$PASS2" ] && break
+        echo
+        echo -e "${lred}---------------------------------------------"
+		echo -e "${lred}Your passwords don´t match, please try again."
+        echo -e "${lred}---------------------------------------------"
+	done
+echo
+echo
+while true
+	do
+		read -s -p "$(echo -e "${grey}Enter your MySQL root password: "${nc})" MYSQL
+		echo
+		read -s -p "$(echo -e "${grey}Enter your MySQL root password (again): "${nc})" MYSQL2
+		[ "$MYSQL" = "$MYSQL2" ] && break
+		echo
+        echo -e "${lred}---------------------------------------------"
+		echo -e "${lred}Your passwords don´t match, please try again."
+        echo -e "${lred}---------------------------------------------"
+	done
+echo
+read -p "$(echo -e "${grey}Enter your public SSH key: "${lpurple})" SSH
+echo
+
+sleep 1
+
+
+
 ## Replace mirrors in sources.list
 echo
 echo
-echo -e "${blue}REPLACING REPOSITORIES"
+echo -e "${lyellow}REPLACING REPOSITORIES"
 echo -e -n "${white}Updating sources.list...${nc}"
 
 echo "
 deb http://httpredir.debian.org/debian jessie main contrib non-free
 deb-src http://httpredir.debian.org/debian jessie main contrib non-free
-
 deb http://httpredir.debian.org/debian jessie-updates main contrib non-free
 deb-src http://httpredir.debian.org/debian jessie-updates main contrib non-free
-
 deb http://security.debian.org/ jessie/updates main contrib non-free
 deb-src http://security.debian.org/ jessie/updates main contrib non-free" > /home/script/sources.list
 
-echo -e "\t\t\t\t\t${white}[${green}DONE${white}]${nc}"
+echo -e "\t\t\t\t\t${white}[${lgreen}DONE${white}]${nc}"
 
 sleep 1
+
+
 
 ## Updating OS to the latest version
 echo
 echo
-echo -e "${blue}UPDATING OPERATING SYSTEM"
+echo -e "${lyellow}UPDATING OPERATING SYSTEM"
 echo -e -n "${white}Downloading package list from repositories... ${nc}"
 #       apt-get update &>/dev/null
-echo -e "\t\t\t${white}[${green}DONE${white}]${nc}"
+echo -e "\t\t\t${white}[${lgreen}DONE${white}]${nc}"
 echo -e -n "${white}Downloading and installing new packages...${nc}"
         apt-get -y upgrade &>/dev/null
-echo -e "\t\t\t${white}[${green}DONE${white}]${nc}"
+echo -e "\t\t\t${white}[${lgreen}DONE${white}]${nc}"
 
 sleep 1
 
-## Network stuff
-echo
-echo
-echo -e "${blue}NETWORK SETTINGS"
-echo -e -n "${white}Changing hostname...${nc}"
-        echo "$hostname" > /home/script/hostname
-echo -e "\t\t\t\t\t\t${white}[${green}DONE${white}]${nc}"
-echo -e -n "${white}Changing FQDN...${nc}"
-        echo "$FQDN" > /home/script/fqdn
-echo -e "\t\t\t\t\t\t${white}[${green}DONE${white}]${nc}"
 
-sleep 1
 
 ## Accounts & SSH
 echo
 echo
 
-HASH=$(openssl passwd -1 -salt temp $DEFAULTPASS)
+HASH=$(openssl passwd -1 -salt temp $PASS)
 
-echo -e "${blue}USER ACCOUNTS AND SSH"
+echo -e "${lyellow}USER ACCOUNTS AND SSH"
 
-echo -e -n "${white}Creating account $USER1...${nc}"
-        useradd $USER1 -s /bin/bash -m -p $HASH &> /dev/null
-echo -e "\t\t\t\t\t${white}[${green}DONE${white}]${nc}"
+echo -e -n "${white}Creating user account...${nc}"
+        useradd $USER -s /bin/bash -m -p $HASH &> /dev/null
+echo -e "\t\t\t\t\t${white}[${lgreen}DONE${white}]${nc}"
 
-echo -e -n "${white}Creating SSH-folder for $USER1...${nc}"
-        mkdir /home/$USER1/.ssh &> /dev/null
-echo -e "\t\t\t\t${white}[${green}DONE${white}]${nc}"
+echo -e -n "${white}Creating SSH-folder...${nc}"
+        mkdir /home/$USER/.ssh &> /dev/null
+echo -e "\t\t\t\t${white}[${lgreen}DONE${white}]${nc}"
 
-echo -e -n "${white}Adding public key for $USER1...${nc}"
-        echo "SSH1" > /home/$USER1/.ssh/authorized_keys
-echo -e "\t\t\t\t\t${white}[${green}DONE${white}]${nc}"
-
-echo -e -n "${white}Creating account $USER2...${nc}"
-        useradd $USER2 -s /bin/bash -m -p $HASH &> /dev/null
-echo -e "\t\t\t\t\t${white}[${green}DONE${white}]${nc}"
-
-echo -e -n "${white}Creating SSH-folder for $USER2...${nc}"
-        mkdir /home/$USER2/.ssh &> /dev/null
-echo -e "\t\t\t\t${white}[${green}DONE${white}]${nc}"
-
-echo -e -n "${white}Adding public key for $USER2...${nc}"
-        echo "$SSH2" > /home/$USER2/.ssh/authorized_keys
-echo -e "\t\t\t\t\t${white}[${green}DONE${white}]${nc}"
+echo -e -n "${white}Adding public key...${nc}"
+        echo "$SSH" > /home/$USER/.ssh/authorized_keys &> /dev/null
+echo -e "\t\t\t\t\t\t${white}[${lgreen}DONE${white}]${nc}"
 
 echo -e -n "${white}Setting folder and file permissions...${nc}"
-        chown $USER1:$USER1 /home/$USER1/.ssh &> /dev/null
-        chown $USER1:$USER1 /home/$USER1/.ssh/authorized_keys &> /dev/null
-        chmod 700 /home/$USER1/.ssh &> /dev/null
-        chmod 600 /home/$USER1/.ssh/authorized_keys &> /dev/null
-        chown $USER2:$USER2 /home/$USER2/.ssh &> /dev/null
-        chown $USER2:$USER2 /home/$USER2/.ssh/authorized_keys &> /dev/null
-        chmod 700 /home/$USER2/.ssh &> /dev/null
-        chmod 600 /home/$USER2/.ssh/authorized_keys &> /dev/null
-echo -e "\t\t\t\t${white}[${green}DONE${white}]${nc}"
-echo -e "${white}___________________________________________________________"
-echo
-echo -e "${white}*** The default password '${lred}$DEFAULTPASS${white}' is being used ***"
-echo -e "${white}___________________________________________________________"
+        chown $USER:$USER /home/$USER/.ssh &> /dev/null
+        chown $USER:$USER /home/$USER/.ssh/authorized_keys &> /dev/null
+        chmod 700 /home/$USER/.ssh &> /dev/null
+        chmod 600 /home/$USER/.ssh/authorized_keys &> /dev/null
+echo -e "\t\t\t\t${white}[${lgreen}DONE${white}]${nc}"
 
-sleep 4
+sleep 1
+
+
 
 ## Installing new software
 echo
 echo
-echo -e "${blue}INSTALLING SOFTWARE"
+echo -e "${lyellow}INSTALLING SOFTWARE"
 echo -e -n "${white}Installing ufw...${nc}"
         apt-get -y install ufw &> /dev/null
-echo -e "\t\t\t\t\t\t${white}[${green}DONE${white}]${nc}"
+echo -e "\t\t\t\t\t\t${white}[${lgreen}DONE${white}]${nc}"
 
 echo -e -n "${white}Installing sudo...${nc}"
-#       apt-get -y install sudo &> /dev/null
-echo -e "\t\t\t\t\t\t${white}[${green}DONE${white}]${nc}"
+       	apt-get -y install sudo &> /dev/null
+echo -e "\t\t\t\t\t\t${white}[${lgreen}DONE${white}]${nc}"
 
 echo -e -n "${white}Installing zip...${nc}"
         apt-get -y install zip &> /dev/null
-echo -e "\t\t\t\t\t\t${white}[${green}DONE${white}]${nc}"
+echo -e "\t\t\t\t\t\t${white}[${lgreen}DONE${white}]${nc}"
 
 echo -e -n "${white}Installing unzip...${nc}"
         apt-get -y install unzip &> /dev/null
-echo -e "\t\t\t\t\t\t${white}[${green}DONE${white}]${nc}"
+echo -e "\t\t\t\t\t\t${white}[${lgreen}DONE${white}]${nc}"
 
 echo -e -n "${white}Installing curl...${nc}"
         apt-get -y install curl &> /dev/null
-echo -e "\t\t\t\t\t\t${white}[${green}DONE${white}]${nc}"
+echo -e "\t\t\t\t\t\t${white}[${lgreen}DONE${white}]${nc}"
 
 echo -e -n "${white}Installing mariadb-server...${nc}"
         export DEBIAN_FRONTEND="noninteractive"
         debconf-set-selections <<< "mariadb-server mysql-server/root_password password $MYSQL"
         debconf-set-selections <<< "mariadb-server mysql-server/root_password_again password $MYSQL"
         apt-get -y install mariadb-server &> /dev/null
-echo -e "\t\t\t\t\t${white}[${green}DONE${white}]${nc}"
+echo -e "\t\t\t\t\t${white}[${lgreen}DONE${white}]${nc}"
 
 echo -e -n "${white}Installing mariadb-client...${nc}"
         apt-get -y install mariadb-client &> /dev/null
-echo -e "\t\t\t\t\t${white}[${green}DONE${white}]${nc}"
+echo -e "\t\t\t\t\t${white}[${lgreen}DONE${white}]${nc}"
 
 echo -e -n "${white}Installing apache2...${nc}"
         apt-get -y install apache2 &> /dev/null
-echo -e "\t\t\t\t\t\t${white}[${green}DONE${white}]${nc}"
+echo -e "\t\t\t\t\t\t${white}[${lgreen}DONE${white}]${nc}"
 
 echo -e -n "${white}Installing php5...${nc}"
         apt-get -y install php5 &> /dev/null
-echo -e "\t\t\t\t\t\t${white}[${green}DONE${white}]${nc}"
+echo -e "\t\t\t\t\t\t${white}[${lgreen}DONE${white}]${nc}"
 
 echo -e -n "${white}Installing libapache2-mod-php5...${nc}"
         apt-get -y install libapache2-mod-php5 &> /dev/null
-echo -e "\t\t\t\t${white}[${green}DONE${white}]${nc}"
+echo -e "\t\t\t\t${white}[${lgreen}DONE${white}]${nc}"
 
 sleep 3
+
+
 
 ## Configuring firewall
 echo
 echo
-echo -e "${blue}CONFIGURING FIREWALL"
+echo -e "${lyellow}CONFIGURING FIREWALL"
+echo -e -n "${white}Configuring firewall for incoming traffic...${nc}"
+        ufw default deny &> /dev/null
+echo -e "\t\t\t${white}[${lgreen}DONE${white}]${nc}"
+
 echo -e -n "${white}Configuring firewall for ssh traffic...${nc}"
         ufw allow ssh &> /dev/null
-echo -e "\t\t\t\t${white}[${green}DONE${white}]${nc}"
+echo -e "\t\t\t\t${white}[${lgreen}DONE${white}]${nc}"
 
 echo -e -n "${white}Configuring firewall for http traffic...${nc}"
         ufw allow http &> /dev/null
-echo -e "\t\t\t${white}[${green}DONE${white}]${nc}"
+echo -e "\t\t\t${white}[${lgreen}DONE${white}]${nc}"
 
 echo -e -n "${white}Configuring firewall for https traffic...${nc}"
         ufw allow https &> /dev/null
-echo -e "\t\t\t${white}[${green}DONE${white}]${nc}"
+echo -e "\t\t\t${white}[${lgreen}DONE${white}]${nc}"
+
+echo -e -n "${white}Activating logging...${nc}"
+        ufw logging on &> /dev/null
+echo -e "\t\t\t\t\t\t${white}[${lgreen}DONE${white}]${nc}"
 
 echo -e -n "${white}Activating firewall...${nc}"
-#        ufw enable &> /dev/null
-echo -e "\t\t\t\t\t\t${white}[${green}DONE${white}]${nc}"
+#	ufw enable &> /dev/null
+echo -e "\t\t\t\t\t\t${white}[${lgreen}DONE${white}]${nc}"
 
 sleep 1
+
+
 
 ## Configuring sudo
 echo
 echo
-echo -e "${blue}CONFIGURING SUDO"
-echo -e -n "${white}Adding $USER1 to the sudoers file...${nc}"
-echo -e "\t\t\t\t${white}[${green}DONE${white}]${nc}"
-
-echo -e -n "${white}Adding $USER2 to the sudoers file...${nc}"
-echo -e "\t\t\t\t${white}[${green}DONE${white}]${nc}"
+echo -e "${lyellow}CONFIGURING SUDO"
+echo -e -n "${white}Adding user to the sudoers file...${nc}"
+echo -e "\t\t\t\t${white}[${lgreen}DONE${white}]${nc}"
 
 sleep 1
+
+
 
 ## Configuring webserver
 echo
 echo
-echo -e "${blue}CONFIGURING WEBSERVER"
+echo -e "${lyellow}CONFIGURING WEBSERVER"
 echo -e -n "${white}Creating folders...${nc}"
         mkdir /var/www/html/web &> /dev/null
         mkdir /var/www/html/tools &> /dev/null
-echo -e "\t\t\t\t\t\t${white}[${green}DONE${white}]${nc}"
+echo -e "\t\t\t\t\t\t${white}[${lgreen}DONE${white}]${nc}"
 
 echo -e -n "${white}Setting folder ownership...${nc}"
         chown www-data:www-data /var/www/html/web &> /dev/null
         chown www-data:www-data /var/www/html/tools &> /dev/null
-echo -e "\t\t\t\t\t${white}[${green}DONE${white}]${nc}"
+echo -e "\t\t\t\t\t${white}[${lgreen}DONE${white}]${nc}"
 
 echo -e -n "${white}Activating apache2 modules...${nc}"
         a2enmod rewrite &> /dev/null
         a2enmod actions &> /dev/null
         a2enmod ssl &> /dev/null
-echo -e "\t\t\t\t\t${white}[${green}DONE${white}]${nc}"
+echo -e "\t\t\t\t\t${white}[${lgreen}DONE${white}]${nc}"
 
 echo -e -n "${white}Modifying apache2.conf for wordpress...${nc}"
-echo -e "\t\t\t\t${white}[${green}DONE${white}]${nc}"
+echo -e "\t\t\t\t${white}[${lgreen}DONE${white}]${nc}"
 
 echo -e -n "${white}Restarting webserver...${nc}"
         service apache2 restart &> /dev/null
-echo -e "\t\t\t\t\t\t${white}[${green}DONE${white}]${nc}"
+echo -e "\t\t\t\t\t\t${white}[${lgreen}DONE${white}]${nc}"
 
 sleep 1
+
+
 
 # Backup
 
